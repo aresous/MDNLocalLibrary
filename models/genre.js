cb8b2eef120924386949815a8beac273447a9b62
aresous <aresous@gmail.com>
2022-01-16 01:41:43 -0500

    routes and controllers established

---
+++ models/genre.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema(
+    {
+        name: {type: String, maxlength:100, minlength:3}
+
+    }
+)
+
+GenreSchema
+.virtual('url')
+.get(()=>{
+    return '/catalog/genre/'+this._id;
+})
+
+module.exports = mongoose.model('Genre', GenreSchema);
