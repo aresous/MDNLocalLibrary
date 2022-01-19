cd49b77112cc66f1740d6edb9f6607f433a896bc
aresous <aresous@gmail.com>
2022-01-18 23:59:23 -0500

    routes added

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-
+const {DateTime} = require('luxon')
 var Schema = mongoose.Schema;
 
 var BookInstanceSchema = new Schema(
@@ -13,9 +13,9 @@ var BookInstanceSchema = new Schema(
 
 // Virtual for bookinstance's URL
 BookInstanceSchema
-.virtual('url')
+.virtual('due_back_formatted')
 .get(function () {
-  return '/catalog/bookinstance/' + this._id;
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 //Export model
